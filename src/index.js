fb95e4a0871ed55c7e3b38a46edabfaf62d1a7e4
NEYROSICK <dhatak24@gmail.com>
2023-06-26 23:56:30 +0300

    issue-resolved

--- src/index.js
+++ src/index.js
@@ -20,29 +20,33 @@ const errorMessageEl = document.querySelector('.error');
 selectBreedEl.setAttribute('data-simplebar', '');
 
 errorMessageEl.classList.add('is-hidden');
+selectBreedEl.classList.add('is-hidden');
 
-fetchBreeds()
-  .then(options => {
-    loadingMessageEl.classList.add('visually-hidden');
-    selectBreedEl.innerHTML = createBreedOptions(options);
-    new SlimSelect({
-      select: '#breed-select',
-    });
-  })
-  .catch(console.warn);
+fetchBreeds().then(options => {
+  loadingMessageEl.classList.add('visually-hidden');
+  selectBreedEl.classList.remove('is-hidden');
+  selectBreedEl.innerHTML = createBreedOptions(options);
+  new SlimSelect({
+    select: '#breed-select',
+  });
+});
 
 const changeHandler = event => {
   if (event.target.value == 'error') {
     return;
   }
 
+  const selectorDivEl = document.querySelector('.ss-main');
+
   catInfoEl.innerHTML = '';
   loadingMessageEl.classList.remove('visually-hidden');
+  selectorDivEl.classList.add('visually-hidden');
 
   setTimeout(() => {
     fetchCatByBreed(event.target.value)
       .then(info => {
         loadingMessageEl.classList.add('visually-hidden');
+        selectorDivEl.classList.remove('visually-hidden');
 
         catInfoEl.innerHTML = createBreedCard(info[0]);
       })
